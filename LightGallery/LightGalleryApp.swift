17ebe86aa9d747d95bf599d3c4ce25c9a90893f5
joyhsin <xinglenyee@gmail.com>
2025-09-07 15:28:22 +0800

    Initial Commit

---
+++ LightGallery/LightGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightGalleryApp.swift
+//  LightGallery
+//
+//  Created by Joy Hsin on 2025/9/7.
+//
+
+import SwiftUI
+
+@main
+struct LightGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
